7bf961dbe0573c0ca9a687c533d3cbc72a99d463
Guilherme Salustiano <guissalustiano@gmail.com>
2022-02-06 14:58:55 -0300

    add click to cli

--- cli.py
+++ cli.py
@@ -1,7 +1,14 @@
 from simulation import Simulation, VcdSimulation
 from table_generator import TableGenerator
+import click
 
-def generate_tikz(vcd_filename):
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('vcd_filename')
+def tikz_timing(vcd_filename):
     from config import timing_config
     simulation: Simulation = VcdSimulation.from_filename(vcd_filename).to_simulation()
     for frame in timing_config.frames:
@@ -11,11 +18,13 @@ def generate_tikz(vcd_filename):
                                  scale=timing_config.scale))
         print('\n')
 
-def generate_table(vcd_filename):
+@cli.command()
+@click.argument('vcd_filename')
+def tabular(vcd_filename):
     from config import table_config
     simulation: Simulation = VcdSimulation.from_filename(vcd_filename).to_simulation()
     table_gen = TableGenerator(simulation, table_config.signals, table_config.time_samples)
     print(table_gen.generate_table())
 
 if __name__ == '__main__':
-    generate_tikz('fluxo_dados.vcd')
+    cli()
